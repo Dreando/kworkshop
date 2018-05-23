8cabade589c20ed7ae32da1177b1695f5a4877f2
kozam <mateusz.koza@tomtom.com>
2018-05-23 11:38:14 +0200

    progress

--- src/main/kotlin/wshop/functions/Operators.kt
+++ src/main/kotlin/wshop/functions/Operators.kt
@@ -2,18 +2,22 @@ package wshop.functions
 
 typealias Ingredient = String
 
-data class Meal(val ingredients: List<Ingredient> = listOf()) {
+data class User(val id: Int, val name: String, val age: Int)
 
-    operator fun plus(otherIngredient: Ingredient): Meal {
-        return Meal(this.ingredients + otherIngredient)
-    }
+class UserRepository {
+
+    val usersDb = listOf(
+            User(1, "Marian", 33),
+            User(2, "Żaneta", 55))
+
+    operator fun get(id: Int): User? = usersDb.filter(...)
+
+    operator fun get(name: String): User? = usersDb.filter(...)
+
+    operator fun get(name: String, agePredicate: (Int) -> Boolean): User? = usersDb.filter(...)
 }
 
 fun main(args: Array<String>) {
 
-    val mojeSniadanie = Meal()
-
-    val jestemJednakGlodny = mojeSniadanie + "Chleb" + "Masełko, ale dobre takie" + "Szyneczka"
-
-    println(jestemJednakGlodny)
+    // TODO finish
 }
