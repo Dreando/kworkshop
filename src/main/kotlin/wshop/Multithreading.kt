cf9cc6ab9fbcb4b1212b63ff790674283422054e
kozam <mateusz.koza@tomtom.com>
2018-06-07 14:48:20 +0200

    coroutines

--- src/main/kotlin/wshop/Multithreading.kt
+++ src/main/kotlin/wshop/Multithreading.kt
@@ -1,2 +1,44 @@
 package wshop
 
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
+
+
+suspend fun complexComputation() {
+    Thread.sleep(2000)
+}
+
+suspend fun veryComplexComputation() {
+    complexComputation()
+    Thread.sleep(1000)
+    println("End of complex computation")
+}
+
+fun main2(args: Array<String>) {
+
+    println("Before launch ")
+
+    launch {
+        veryComplexComputation()
+    }
+
+    //Thread.sleep(4000)
+
+    println("After launch ")
+}
+
+fun main(args: Array<String>) = runBlocking {
+
+    println("Before async ")
+
+    val deferredResult = async {
+        veryComplexComputation()
+    }
+
+    deferredResult.await()
+
+    //Thread.sleep(4000)
+
+    println("After async ")
+}
